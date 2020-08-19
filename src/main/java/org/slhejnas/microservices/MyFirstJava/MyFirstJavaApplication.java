c3dcbb55217fc1cd6d3c2723482b7c1096a5f61e
Steven Hejnas <49131468+SHejnas@users.noreply.github.com>
2020-08-19 17:50:06 -0400

    Initial commit

---
+++ src/main/java/org/slhejnas/microservices/MyFirstJava/MyFirstJavaApplication.java
@@ -0,0 +1,13 @@
+package org.slhejnas.microservices.MyFirstJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/slhejnas/microservices/MyFirstJava/MyFirstJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.slhejnas.microservices.MyFirstJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
