002bdc2c937794843cd96425b8d7b73c3000e59c
Steven Hejnas <49131468+SHejnas@users.noreply.github.com>
2020-09-04 16:19:58 -0400

    major overhaul of controller and service, response is in place with checked exceptions

--- src/main/java/org/slhejnas/microservices/MyFirstJava/service/ToDoService.java
+++ src/main/java/org/slhejnas/microservices/MyFirstJava/service/ToDoService.java
@@ -1,52 +1,70 @@
 package org.slhejnas.microservices.MyFirstJava.service;
 
 import lombok.extern.slf4j.Slf4j;
-import org.slhejnas.microservices.MyFirstJava.model.ResponseWrapper;
+import org.slhejnas.microservices.MyFirstJava.exception.ToDoNotFoundException;
+import org.slhejnas.microservices.MyFirstJava.model.ToDoResponse;
 import org.slhejnas.microservices.MyFirstJava.model.ToDo;
 import org.slhejnas.microservices.MyFirstJava.repository.ToDoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+
 @Slf4j
 @Service
 public class ToDoService {
     private final ToDoRepository toDoRepository;
+
     @Autowired
-    public ToDoService (ToDoRepository toDoRepository){
+    public ToDoService(ToDoRepository toDoRepository) {
         this.toDoRepository = toDoRepository;
     }
-    public ToDo insertToDo(ToDo toDo){
-        return toDoRepository.save(toDo);
-    }
-    public List<ToDo> getAllToDo(){
-        return toDoRepository.findAll();
-    }
-    public Optional<ToDo> getOneToDo(Long id){
-        Optional <ToDo> toDoOptional = toDoRepository.findById(id);
-        log.debug("returning: {}", toDoOptional.orElseGet(() -> null));
-        return toDoOptional;
+
+    public ToDo insertToDo(ToDo toDo) throws ToDoNotFoundException{
+            return toDoRepository.save(toDo);
     }
 
-    public Optional<ToDo> toggleToDo(Long id){
+    public List<ToDo> getAllToDo() throws ToDoNotFoundException {
+        List<ToDo> toDoList = toDoRepository.findAll();
+        if (!toDoList.isEmpty()){
+            return toDoList;
+        }else{
+            throw new ToDoNotFoundException();
+        }
+    }
+
+    public ToDo getOneToDo(Long id) throws ToDoNotFoundException {
+        Optional<ToDo> toDoOptional =  toDoRepository.findById(id);
+        if (toDoOptional.isPresent()) {
+            return toDoOptional.get();
+        } else {
+            throw new ToDoNotFoundException();
+        }
+    }
+
+    public ToDo toggleToDo (Long id) throws ToDoNotFoundException{
 
         Optional<ToDo> toDoOptional = toDoRepository.findById(id);
-        if(toDoOptional.isPresent()) {
+        if (toDoOptional.isPresent()) {
 
             ToDo toDo = toDoOptional.get();
             toDo.setCompleted(!toDo.getCompleted());
             toDoRepository.save(toDo);
+            return  toDo;
+        }else {
+            throw new ToDoNotFoundException();
         }
-        return toDoOptional;
     }
-    public ResponseWrapper deleteToDo(Long id){
+    public void deleteToDo (Long id) throws ToDoNotFoundException {
         Optional<ToDo> toDoOptional = toDoRepository.findById(id);
-        if(toDoOptional.isPresent()) {
+        if (toDoOptional.isPresent()) {
             toDoRepository.deleteById(id);
-            return new ResponseWrapper(true, 200, "successfully deleted", null);
-        }else{
-            return new ResponseWrapper(false, 404, "Not Found", null);
+        } else {
+            throw new ToDoNotFoundException();
         }
     }
+
 }
